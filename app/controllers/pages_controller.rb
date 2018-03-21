f388dabd5bf896b636f25a15057b0d912dd5582a
Uthaeus <romanlavery@gmail.com>
2018-03-21 16:08:58 -0600

    bundle update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,20 @@
 class PagesController < ApplicationController
+
   def home
-    @posts = Blog.all
-    
+    @posts = Blog.all 
+    @titles = Blog.all  
   end
 
   def titles
+    @titles = Blog.all
   end
 
   def authors
+    @authors = Blog.all
   end
 
   def subjects
+    @subjects = Blog.all
   end
+
 end
